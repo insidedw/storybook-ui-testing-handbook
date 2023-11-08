25b4a4792a450ffcf029cad9b9d607a5d0766f3a
김대웅[JP플레이스서비스개발] <sh.daewoong@navercorp.com>
2023-11-08 14:21:01 +0900

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import InboxScreen from "./InboxScreen";
 import LoginScreen from "./LoginScreen";
 import "./index.css";
 function App() {
+  //hi
   const [user, logIn] = useAuth();
 
   return <>{user?.token ? <InboxScreen /> : <LoginScreen onLogIn={logIn} />}</>;
